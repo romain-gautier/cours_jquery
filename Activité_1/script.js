34ceb6af25d7ed671a64d0296d2beccfd751ed14
Romain Gautier <romain.photog@gmail.com>
2016-02-12 08:40:22 +0100

    mise en forme du formulaire

--- Activité_1/script.js
+++ Activité_1/script.js
@@ -4,6 +4,7 @@ $(function() {
     $('hr').after('<div id="textLabel">Texte du label <input type="text" id="idLabel"> <button id="okLabel" class="btn btn-default">Ok</button></div>');
     $('#okLabel').click(function(){
       $('#gauche').append('<span><strong>' + $('#idLabel').val() + '</strong></span>');
+      $('span').append('   ');
       $('#textLabel').remove();
     });
   });
@@ -12,7 +13,7 @@ $(function() {
   $('#buttonZone').click(function(){
     $('hr').after('<div id="textZone">id zone de texte <input type="text" id="idZone"> <button id="okZone" class="btn btn-default">Ok</button></div>');
     $('#okZone').click(function(){
-      $('#gauche').append('<input id="'+ $('#idZone').val() + '" type="text"><br>');
+      $('#gauche').append('<input id="'+ $('#idZone').val() + '" type="text"><br><br>');
       $('#textZone').remove();
     });
   });
@@ -21,7 +22,7 @@ $(function() {
   $('#buttonBouton').click(function(){
     $('hr').after('<div id="textBouton">Texte bouton <input type="text" id="idBouton"> <button id="okBouton" class="btn btn-default">Ok</button></div>');
     $('#okBouton').click(function(){
-      $('#gauche').append('<button class="btn btn-primary">' + $('#idBouton').val() + '</button><br>');
+      $('#gauche').append('<button class="btn btn-primary">' + $('#idBouton').val() + '</button><br><br>');
       $('#textBouton').remove();
     });
   });
