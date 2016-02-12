b68cbe1318a9de3fbd36799ea8ff9682980b57e1
Romain Gautier <romain.photog@gmail.com>
2016-02-12 01:10:20 +0100

    activité 1

--- Activité_1/script.js
+++ Activité_1/script.js
@@ -1,3 +1,29 @@
 $(function() {
-  // Insérer le code jQuery ici
+  // Lorsque l'on click sur label
+  $('#buttonLabel').click(function(){
+    $('hr').after('<div id="textLabel">Texte du label <input type="text" id="idLabel"> <button id="okLabel" class="btn btn-default">Ok</button></div>');
+    $('#okLabel').click(function(){
+      $('#gauche').append('<span><strong>' + $('#idLabel').val() + '</strong></span>');
+      $('#textLabel').remove();
+    });
+  });
+
+  // Lorsque l'on click sur zone de texte
+  $('#buttonZone').click(function(){
+    $('hr').after('<div id="textZone">id zone de texte <input type="text" id="idZone"> <button id="okZone" class="btn btn-default">Ok</button></div>');
+    $('#okZone').click(function(){
+      $('#gauche').append('<input id="'+ $('#idZone').val() + '" type="text"><br>');
+      $('#textZone').remove();
+    });
+  });
+
+  // Lorsque l'on click sur Bouton
+  $('#buttonBouton').click(function(){
+    $('hr').after('<div id="textBouton">Texte bouton <input type="text" id="idBouton"> <button id="okBouton" class="btn btn-default">Ok</button></div>');
+    $('#okBouton').click(function(){
+      $('#gauche').append('<button class="btn btn-primary">' + $('#idBouton').val() + '</button><br>');
+      $('#textBouton').remove();
+    });
+  });
+
   });
