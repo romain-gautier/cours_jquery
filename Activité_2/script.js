bd87b1436fa5eb35e8bb1856ed7857caf57f0b45
Romain Gautier <romain.photog@gmail.com>
2016-02-14 21:28:03 +0100

    ajout des commentaires

--- Activité_2/script.js
+++ Activité_2/script.js
@@ -4,11 +4,11 @@ $(function() {
   $('#buttonLabel').click(function(){
     $('hr').after('<div class ="apparition" id="textLabel">Texte du label <input type="text" id="idLabel"> <button id="okLabel" class="btn btn-default">Ok</button> <button id="nulLabel" class="btn btn-default">Annuler</button></div>');
     $('.apparition').fadeIn('slow'); // apparition de la zone de saisie qui n'est pas apparente par défaut dans le CSS (display: none)
-    $('#buttonLabel, #buttonZone, #buttonBouton').prop("disabled", true);
+    $('#buttonLabel, #buttonZone, #buttonBouton').prop("disabled", true); // on désactive les boutons lable, zone de texte et bouton
     $('#okLabel').click(function(){
       $('#gauche').append('<span><strong>' + $('#idLabel').val() + '</strong></span>');
       $('span').append('   ');
-      $('#buttonLabel, #buttonZone, #buttonBouton').prop("disabled", false);
+      $('#buttonLabel, #buttonZone, #buttonBouton').prop("disabled", false); // on réactive les boutons lable, zone de texte et bouton
       $('.apparition').fadeOut('slow', function() { // disparition de la zone de saisie dans la partie droite avec fadeOut
         $('#textLabel').remove();
       });
@@ -16,7 +16,7 @@ $(function() {
     // si click sur annuler :
     $('#nulLabel').click(function() {
       $('#textLabel').remove();
-      $('#buttonLabel, #buttonZone, #buttonBouton').prop("disabled", false);
+      $('#buttonLabel, #buttonZone, #buttonBouton').prop("disabled", false); // on réactive les boutons lable, zone de texte et bouton
     });
   });
 
@@ -24,10 +24,10 @@ $(function() {
   $('#buttonZone').click(function(){
     $('hr').after('<div class="apparition" id="textZone">id zone de texte <input type="text" id="idZone"> <button id="okZone" class="btn btn-default">Ok</button> <button id="nulLabel" class="btn btn-default">Annuler</button></div>');
     $('.apparition').fadeIn('slow'); // apparition de la zone de saisie qui n'est pas apparente par défaut dans le CSS (display: none)
-    $('#buttonLabel, #buttonZone, #buttonBouton').prop("disabled", true);
+    $('#buttonLabel, #buttonZone, #buttonBouton').prop("disabled", true); // on désactive les boutons lable, zone de texte et bouton
     $('#okZone').click(function(){
       $('#gauche').append('<input id="'+ $('#idZone').val() + '" type="text" placeholder="'+ $('#idZone').val() + '"><br><br>');
-      $('#buttonLabel, #buttonZone, #buttonBouton').prop("disabled", false);
+      $('#buttonLabel, #buttonZone, #buttonBouton').prop("disabled", false); // on réactive les boutons lable, zone de texte et bouton
       $('.apparition').fadeOut('slow', function() { // disparition de la zone de saisie dans la partie droite avec fadeOut
         $('#textZone').remove();
       });
@@ -35,7 +35,7 @@ $(function() {
     // si click sur annuler :
     $('#nulLabel').click(function() {
       $('#textZone').remove();
-      $('#buttonLabel, #buttonZone, #buttonBouton').prop("disabled", false);
+      $('#buttonLabel, #buttonZone, #buttonBouton').prop("disabled", false); // on réactive les boutons lable, zone de texte et bouton
     });
   });
 
@@ -43,10 +43,10 @@ $(function() {
   $('#buttonBouton').click(function(){
     $('hr').after('<div class = "apparition" id="textBouton">Texte bouton <input type="text" id="idBouton"> <button id="okBouton" class="btn btn-default">Ok</button> <button id="nulLabel" class="btn btn-default">Annuler</button></div>');
     $('.apparition').fadeIn('slow'); // apparition de la zone de saisie qui n'est pas apparente par défaut dans le CSS (display: none)
-    $('#buttonLabel, #buttonZone, #buttonBouton').prop("disabled", true);
+    $('#buttonLabel, #buttonZone, #buttonBouton').prop("disabled", true); // on désactive les boutons lable, zone de texte et bouton
     $('#okBouton').click(function(){
       $('#gauche').append('<button class="btn btn-primary">' + $('#idBouton').val() + '</button><br><br>');
-      $('#buttonLabel, #buttonZone, #buttonBouton').prop("disabled", false);
+      $('#buttonLabel, #buttonZone, #buttonBouton').prop("disabled", false); // on réactive les boutons lable, zone de texte et bouton
       $('.apparition').fadeOut('slow', function() { // disparition de la zone de saisie dans la partie droite avec fadeOut
         $('#textBouton').remove();
       });
@@ -54,7 +54,7 @@ $(function() {
     // si click sur annuler :
     $('#nulLabel').click(function() {
       $('#textBouton').remove();
-      $('#buttonLabel, #buttonZone, #buttonBouton').prop("disabled", false);
+      $('#buttonLabel, #buttonZone, #buttonBouton').prop("disabled", false); // on réactive les boutons lable, zone de texte et bouton
     });
   });
 
